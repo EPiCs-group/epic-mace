48d2b327d5f42e006a51a0c2443c183cf3e7b952
Ivan Chernyshov <ivan-chernyshoff@yandex.ru>
2021-01-30 22:11:53 +0300

    Small fixes

--- manual/examples/05_constrained_embedding/constrained_embedding.py
+++ manual/examples/05_constrained_embedding/constrained_embedding.py
@@ -4,19 +4,17 @@ Direct 3D generation of all complexes
 
 import mace
 
+#%% Prepare cores
+
 # main ligand
 ligand_smiles = '[*][P:1]([*])[C@@H]1CCCC[C@@H]1C[N-:1]CCN1C=C[N+]([*])=[C-:1]1 |r,$_R1;;_R1;;;;;;;;;;;;;;;_R2;$,c:15,18|'
-L = mace.MolFromSmiles(ligand_smiles)
 
 # subs
-Rs = {'core': {'R1': mace.MolFromSmiles('[*][H]'),
-               'R2': mace.MolFromSmiles('[*][H]')},
-      'deriv': {'R1': mace.MolFromSmiles('[*]CC'),
-                'R2': mace.MolFromSmiles('[*]c1c(C)cc(C)cc(C)1')}}
+Rs = {'R1': mace.MolFromSmiles('[*]CC'),
+      'R2': mace.MolFromSmiles('[*]c1c(C)cc(C)cc(C)1')}
 
 # core complex
-ligands = [mace.MolToSmiles(mace.AddSubsToMol(L, Rs['core'])),
-           '[C-:1]#[O+]', '[C-:1]#[O+]', '[C-:1]#[O+]']
+ligands = [ligand_smiles, '[C-:1]#[O+]', '[C-:1]#[O+]', '[C-:1]#[O+]']
 CA = '[Mn+]'
 geom = 'OH'
 X = mace.ComplexFromLigands(ligands, CA, geom)
@@ -26,7 +24,12 @@ Xs = X.GetStereomers()
 
 # generate conformers
 for i, X in enumerate(Xs):
-    X.AddConformers(numConfs = 50, rmsThresh = 1.0)
+    X.AddConformers(numConfs = 20, rmsThresh = 1.0)
     X.ToXYZ(f'X{i}.xyz', 'all')
+
+
+#%% Constrained embedding
+
+
 
 
